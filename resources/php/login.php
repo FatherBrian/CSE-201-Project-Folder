a193bd4fba2c6ff9c2e74ef8171588fcf491a503
kanena <kanena@miamioh.edu>
2018-03-26 14:53:04 -0400

    Edits to login class; Integrating mySQL to db

--- resources/php/login.php
+++ resources/php/login.php
@@ -3,9 +3,9 @@ class login {
 
 	function connect() {
 		$dbhost = "";
-		$dbuser = "akpsfzgp_akpsiLA";
-		$dbpass = "akpsihonor&integrity!";
-		$dbname = "akpsfzgp_akpsight";
+		$dbuser = "root";
+		$dbpass = "";
+		$dbname = "collegebook";
 		$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 		// Testing connection success/failure
 		if(mysqli_connect_errno()) {
@@ -18,14 +18,14 @@ class login {
 		mysqli_close($connection);
 	}
 
-	function grab($connection, $query) {		
+	function grab($connection) {		
+		$query = "Select * From users Where username = 'nick'";
 		$result = mysqli_query($connection, $query); // True or false if query works and finds record
 		if ($result && mysqli_affected_rows($connection) == 1) { 	// Tests if anything was found && if something was successfully changed with connection 
 			$array = mysqli_fetch_array($result); // Gets all values needed from specific query according to name
-
+			echo "This worked!";
 			$_SESSION['logged'] = "Yes";
 		}
 	}
-
-	
+}	
 ?>
--- website/index.php
+++ website/index.php
@@ -2,11 +2,15 @@
 	session_start();
 //	$server = $_SERVER["DOCUMENT_ROOT"] . "/resources/php/Main.php"
 	$server = "../resources/php/page.php";
+	$login = "../resources/php/login.php";
 	include($server);
+	include($login);
 //	$message = login_start(); // Gets login procedure, and also returns value that comes out of login function
 //	start("Login", "none", 0);
 	$gen = new page();
+	$log = new login();
 	$gen->head();
+	$connection = $log->connect();
 ?>
 
 <?php $gen->title(); ?>
@@ -17,7 +21,7 @@
 <div class="row">
 	<div class="col-xs-3">
 		<h1> Login </h1>
-		<form id="Download1" action="<?php login() ?>" method="post">
+		<form id="Download1" action="<?php $log->grab($connection) ?>" method="post">
 			Username: <input id="Download_Code" type="text" name="username" value="">  </br></br>
 			Password: <input id="Download_Code" type="password" name="password" value=""> </br></br>
 			<input id="Download_File" type="submit" name="submit" value="Login">
