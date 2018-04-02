0fee2e9d05908534c4ba53e6c7b7b6d8809a9fae
kanena <kanena@miamioh.edu>
2018-04-01 21:00:22 -0400

    Making edits for Login functionality on front-end webpage

--- resources/php/message.php
+++ resources/php/message.php
@@ -2,16 +2,7 @@
 class message {
 
 	function connect() {
-		$dbhost = "";
-		$dbuser = "akpsfzgp_akpsiLA";
-		$dbpass = "akpsihonor&integrity!";
-		$dbname = "akpsfzgp_akpsight";
-		$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-		// Testing connection success/failure
-		if(mysqli_connect_errno()) {
-			die("Database connection failed: " . mysqli_connect_error() . " (" . mysqli_connect_errno() . ")"); 
-		}
-		return $connection;
+
 	}
 
 
--- website/index.php
+++ website/index.php
@@ -1,12 +1,9 @@
 <?php
 	session_start();
-//	$server = $_SERVER["DOCUMENT_ROOT"] . "/resources/php/Main.php"
 	$server = "../resources/php/page.php";
 	$login = "../resources/php/login.php";
 	include($server);
 	include($login);
-//	$message = login_start(); // Gets login procedure, and also returns value that comes out of login function
-//	start("Login", "none", 0);
 	$gen = new page();
 	$log = new login();
 	$gen->head();
@@ -21,7 +18,7 @@
 <div class="row">
 	<div class="col-xs-3">
 		<h1> Login </h1>
-		<form id="Download1" action="<?php $log->grab($connection) ?>" method="post">
+		<form id="Download1" action="<?php $log->grab($connection, $_POST["username"], $_POST["password"])) ?>" method="post">
 			Username: <input id="Download_Code" type="text" name="username" value="">  </br></br>
 			Password: <input id="Download_Code" type="password" name="password" value=""> </br></br>
 			<input id="Download_File" type="submit" name="submit" value="Login">
