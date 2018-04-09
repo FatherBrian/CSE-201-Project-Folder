8cc4bc516b21b698e6a5b5fe659402189d72e03b
Brian <brfother@gmail.com>
2018-04-08 22:29:22 -0400

    Fixed Update of Profile class and webpage files

--- website/profile.php
+++ website/profile.php
@@ -1,49 +1,29 @@
-<?php>
+<?php
 
-function connect() {
-		$dbhost = "";
-		$dbuser = "root";
-		$dbpass = "";
-		$dbname = "collegebook";
-		$connection = mysql_connect($dbhost, $dbuser, $dbpass, $dbname);
-		// Testing connection success/failure
-		if(mysql_connect_errno()) {
-			die("Database connection failed: " . mysql_connect_error() . " (" . mysql_connect_errno() . ")"); 
-		}
-		return $connection;
-	}
-
-	function close($connection) {
-		mysql_close($connection);
-	}
-
-mysql_select_db(“user”)
-
-$userid = $_REQUEST(‘user ID’);
-
-$get = mysql_query(“SELECT * FROM userid WHERE userid = ‘$user ID’”);
-$get2 = mysql_fetch_assoc($get);
-$username = $get2 (‘user Name’)
-
+session_start();
+	$db = "../resources/php/database.php";
+	$page = "../resources/php/page.php";
+	$profile = "../resources/php/profile.php";
+	include($db);
+	include($page);
+	include($home);
+	$server = new database();
+	$gen = new page();
+	$profile = new profile();
+	$gen->head();
+	$connection = $server->connect();
 ?>
 
-<html>
-	<head>
-		<title> Profile Page:</title>
-	</head>
-<center>
-<body>
-<br/>
-<br/>
-<br/>
-<br/>
+<?php $gen->title(); ?>
 
-ID: <b><?php echo $userID; ?></b> 
-User Name: <b><?php echo $username; ?></b> 
-Password: <b><?php echo $Password; ?></b> 
-E-mail: <b><?php echo $e-mail; ?></b> 
+<?php $gen->nav(); ?>
 
+
+
+<?php $profile->generateProfile($connection); ?>
+
+<?php $gen->footer(); ?>
 
 </body>
-</center>
 </html>
+
